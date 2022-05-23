4ba6956d49752f2d0cdc73903b86a34c225934ef
dahanzimin <353767514@qq.com>
2022-05-23 15:38:18 +0800

    Fix possible I2C_ try_ Lock problem

--- mixgoce_lib/mixgoce.py
+++ mixgoce_lib/mixgoce.py
@@ -1,19 +1,8 @@
 # Paste mixgoce.py
-# Optimize requests  	—— dahanzmin 2205012
 
 import board
 from rtc import RTC
 from busio import I2C
-
-def do_connect(username, password):
-	import wifi
-	from time import sleep
-	
-	wifi.radio.connect(ssid=username.encode(), password=password.encode())
-	while not wifi.radio.ipv4_address:
-		sleep(0.1)
-	print("Wi-Fi connected!")
-	return wifi.radio
 
 rtc_clock = RTC()
 
@@ -21,29 +10,25 @@ try:
 	i2c = I2C(board.IO41, board.IO42,frequency=400000)
 	while not i2c.try_lock():
 		pass
-	if 0x6a in i2c.scan():
-		version = 99
-	elif 0x15 in i2c.scan():
+		
+	_scan=i2c.scan()
+	if 0x15 in _scan:
 		version = 1
-	else:
+	elif 0x26 in _scan:
 		version = 0
-		
+	else:
+		raise AttributeError("There is a problem with ACC")
+	
+finally:
 	i2c.unlock()
-	
-	if version == 99:
-		i2c.deinit()
-	elif version:#new
-		import mxc6655xa
-		acc = mxc6655xa.MXC6655XA(i2c)
-	
-		def get_temperature():
-			return acc.get_temperature()
-	else:#old
-		import msa301
-		acc = msa301.MSA301(i2c)
-		
-except Exception as e:
-	print(e)
+
+if version:#new
+	import mxc6655xa
+	acc = mxc6655xa.MXC6655XA(i2c)
+
+else:#old
+	import msa301
+	acc = msa301.MSA301(i2c)
 
 ADDITIONAL_TOPIC = 'b640a0ce465fa2a4150c36b305c1c11b'
 WILL_TOPIC = '9d634e1a156dc0c1611eb4c3cff57276'
